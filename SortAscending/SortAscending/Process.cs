1453fa25ed38031f426c7c3f95c66269510899b4
ThanhPhongsof <ttphongletter@gmail.com>
2022-02-27 11:51:01 +0700

    commit code sort basic

---
+++ SortAscending/SortAscending/Process.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAscending
+{
+    public class Process
+    {
+        public static void Show(int[] arr)
+        {
+            arr.ToList().ForEach(x => { Console.Write(x + "   "); });
+            Console.WriteLine();
+        }
+
+        public static int GetValue()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        public static void SortAsc_Basic(int[] arr)
+        {
+            Console.WriteLine($"Sort Asc array: ");                            // Sort asc
+            Array.Sort(arr);
+            Show(arr);
+        }
+
+        public static void SortDesc_Basic(int[] arr)
+        {
+            Console.WriteLine($"Sort Asc array: ");                            // Sort desc
+            Array.Reverse(arr);
+            Show(arr);
+        }
+
+        public static void SortArrayAsc(int[] arr)
+        {
+            QuickSort(arr, 0, arr.Length - 1);
+        }
+
+        public static void SortArrayDesc(int[] arr)
+        {
+            QuickSortv2(arr, 0, arr.Length - 1);
+        }
+
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            int i = left;
+            int j = right;
+
+            var pivot = arr[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (arr[i] < pivot) 
+                    i++;
+
+                while (arr[j] > pivot) 
+                    j--;
+
+                if (i <= j)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j) 
+                QuickSort(arr, left, j);
+
+            if (i < right) 
+                QuickSort(arr, i, right);
+        }
+
+        public static void QuickSortv2(int[] arr, int left, int right)
+        {
+            int i = left;
+            int j = right;
+
+            var pivot = arr[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (arr[i] < pivot)
+                    i++;
+
+                while (arr[j] > pivot)
+                    j--;
+
+                if (i >= j)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left > j)
+                QuickSortv2(arr, left, j);
+
+            if (i < right)
+                QuickSortv2(arr, i, right);
+        }
+    }
+}
---
+++ SortAscending/SortAscending/Program.cs
@@ -0,0 +1,28 @@
+﻿using SortAscending;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter element into the array:");
+int n = Process.GetValue();                                                 // Get Value
+
+int[] arr = new int[n];
+for (int i = 0; i < n; i++)                                                 // Loop
+{
+    Console.WriteLine($"Value {i}: ");
+    arr[i] = Process.GetValue();
+}
+Console.WriteLine($"Array: ");                                              // Array no sort
+Process.Show(arr);
+
+//Process.SortAsc_Basic(arr);
+//Process.SortDesc_Basic(arr);
+
+//Console.WriteLine($"Sort Asc array by QuickSort: ");                         // Array
+//Process.SortArrayAsc(arr);
+//Process.Show(arr);
+
+Console.WriteLine($"Sort Asc array by QuickSort: ");                         // Array
+Process.SortArrayDesc(arr);
+Process.Show(arr);
+
+
+
